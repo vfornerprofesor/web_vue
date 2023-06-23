1ef532a51a0f775f45c5a116ac68051334505299
vfornerprofesor <vforner.profesor@gmail.com>
2023-06-23 11:33:02 +0200

    Imatges arreglades

--- src/components/blocks/Unit.vue
+++ src/components/blocks/Unit.vue
@@ -2,7 +2,8 @@
   
   <router-link class="unit-link" :to="link_page">
         <div class="unit">
-            <img :src="link_img" alt="" class="unit-img img-fluid">
+            <!--<img :src="link_img" alt="" class="unit-img img-fluid">-->
+            <img :src="getImageLink(link_img)" alt="" class="unit-img img-fluid" />
             <br>
             <button v-if="title" class="btn btn-primary unit-title">{{ title }}</button>
         </div>
@@ -11,6 +12,9 @@
 </template>
    
    <script>
+   import image1 from '@/img/logo.png';
+   import image2 from '@/img/css.png';
+   import image3 from '@/img/html.png';
    export default {
      name: 'UnitComponent',
      props: {
@@ -22,6 +26,23 @@
     },
     title: {
       type: String
+    }
+  },
+  methods: {
+    getImageLink(imageName) {
+      // Array con los nombres de las imágenes y sus enlaces correspondientes
+      const imageLinks = [
+        { name: 'imagen1', link: image1 },
+        { name: 'imagen2', link: image2 },
+        { name: 'imagen3', link: image3 },
+        // Agrega más elementos al array según necesites
+      ];
+
+      // Busca el enlace de la imagen según su nombre
+      const image = imageLinks.find(image => image.name === imageName);
+
+      // Devuelve el enlace de la imagen o un enlace por defecto si no se encuentra
+      return image ? image.link : 'ruta/a/imagen-por-defecto.jpg';
     }
   }
      // Puedes agregar métodos o lógica adicional aquí si es necesario
